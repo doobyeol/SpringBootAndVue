41f5dd73ec07b307b9260874a8c859af935c45cf
Doobyeol <doobyeol@gmail.com>
2022-02-28 17:27:34 +0900

    Spring boot + Vue + Quasar

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+    // https://cli.vuejs.org/config/#devserver-proxy
+    devServer: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    }
+}
